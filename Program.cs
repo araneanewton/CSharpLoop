cb7ba3f6026dad8284ac4a5f5095d8f44f5588c4
araneanewton <araneanewton@gmail.com>
2025-05-17 21:11:46 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿public class Program
 {
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        //
+        int sum = 0;
+        for (int i = 0; i < 20; i++)
+        {
+            if (myArray[i] % 2 != 0)
+            {
+                sum += myArray[i];
+            }
+        }
+        Console.WriteLine(sum);
     }
 }
