aa78947ce9c5892c96d005d295e7786ff8c62435
araneanewton <araneanewton@gmail.com>
2025-05-17 20:55:09 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,6 @@
 {
     private static void Main(string[] args)
     {
-        int[] myArrays = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        foreach (int item in myArrays)
-        {
-            Console.WriteLine(item);
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
